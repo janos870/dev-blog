c53a80a8c0c1c31fbbce186a13cbe5576f54686b
janos870 <janos.balogh870@gmail.com>
2023-11-29 23:37:58 +0100

    Change the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
+import { InferGetStaticPropsType, NextPage } from "next";
+import BlogCard from "@/components/BlogCard";
+import { readPostInfo } from "@/lib/helper";
+import { PostApiResponse } from "@/utils/types";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const postInfo: PostApiResponse = readPostInfo();
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
-    <main>
-      <h1 className="text-3xl text-gray-400 font-bold underline">Hello world!</h1>
-    </main>
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+      {posts.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
+      ))}
+    </div>
   );
-}
+};
+
+export default Blogs;
